92f21c44869317a0448c6b8bf38ad7249d534fb3
Keiran <127797224+GitHubWestie@users.noreply.github.com>
2024-02-28 08:25:27 +0000

    Added code to check answer and respond to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     for (let button of buttons) {
         button.addEventListener('click', function() {
             if (this.getAttribute('data-type') === 'submit') {
-                alert('You clicked submit!');
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute('data-type');
                 runGame(gameType);
@@ -32,10 +32,25 @@ function runGame(gameType) {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
+
+    runGame(calculatedAnswer[1]);
 }
 
-function checkAnswer() {
 
+/**
+ * Checks the answer against the first item in 
+ * the calculateCorrectAnswer array
+ */
+function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert('Hey! You got it right!');
+    } else {
+        alert(`Awww... You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}`)
+    }
 }
 
 /**Gets the operands and the operators 
